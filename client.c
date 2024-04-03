9be1c3e89336cf14c9e81a58259229e661cb1311
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-04-03 12:16:10 -0700

    added parsing to client

--- client.c
+++ client.c
@@ -4,26 +4,25 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-
-#define SERVER_IP "127.0.0.1" 
-#define SERVER_PORT 8080 
+#include "cJSON.h"
+#include "config.h"
 
 // Creates a TCP socket and intializes connection
 // Returns socket file descriptor
-int connect_to_server() {
+int connect_to_server(ConfigData* config_data) {
     // Create a TCP socket
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
-        perror("socket");
-        return 1;
+        perror("Error creating socket");
+        return -1;
     }
 
     // Set up the server address
     struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(config_data->server_ip_addr);
+    server_addr.sin_port = htons(config_data->tcp_pre_probe);
 
     // Connect to the server
     if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
@@ -35,14 +34,8 @@ int connect_to_server() {
 
 // Sends a file via TCP to the server
 // Returns socket file descriptor
-int send_config(char* file_path, int sock) {
-    // Open the file
-    FILE *file = fopen(file_path, "rb");
-    if (file == NULL) {
-        perror("Error opening config file");
-        close(sock);
-        return -1;
-    }
+int send_config(FILE* file, int sock) {
+    rewind(file);
 
     // Send the file to the server
     char buffer[1024];
@@ -51,7 +44,6 @@ int send_config(char* file_path, int sock) {
         if (send(sock, buffer, bytes_read, 0) < 0) {
             perror("Error sending config file");
             fclose(file);
-            close(sock);
             return -1;
         }
     }
@@ -69,18 +61,37 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
+    // Open the file
+    char *file_path = argv[1];
+    FILE *file = fopen(file_path, "rb");
+    if (file == NULL) {
+        perror("Error opening config file");
+        return -1;
+    }
+
+    // Parse and extract config data
+    ConfigData* config_data = malloc(sizeof(config_data));
+    if (config_data == NULL) {
+        perror("Error allocating config data");
+        return -1;
+    }
+    cJSON* json_root = parse_config(file);
+    extract_config(config_data, json_root);
+
+
     // Create socket and connect to server
-    int sock = connect_to_server();
+    int sock = connect_to_server(config_data);
     if (sock < 0) {
         perror("Error connecting to server");
+        close(sock);
         return -1;
     }
 
     // Send config file to server
-    char *file_path = argv[1];
-    sock = send_config(file_path, sock);
+    sock = send_config(file, sock);
     if (sock < 0) {
         perror("Unable to send config file");
+        close(sock);
         return -1;
     }
 
