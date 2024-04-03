5492e91a12f51f2b004acd0f2fd2936063e90939
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-04-03 12:16:41 -0700

    used config data in setup and moved shared functions to config

--- server.c
+++ server.c
@@ -5,27 +5,11 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include "cJSON.h"
-
-#define SERVER_PORT 8080 // Replace with the port number to listen on
-
-// Stores configuration information
-typedef struct {
-    char server_ip_addr[16];
-    int udp_source_port;
-    int udp_destination_port;
-    int tcp_head_syn;
-    int tcp_tail_syn;
-    int tcp_pre_probe;
-    int tcp_post_probe;
-    int udp_payload_size;
-    int inter_measurement_time;
-    int num_udp_packets;
-    int ttl;
-} ConfigData;
+#include "config.h"
 
 // Creates a TCP socket, binds to a port, and listens for incoming connections
 // Returns server socket file descriptor
-int bind_and_listen() {
+int bind_and_listen(int port) {
     // Create a TCP socket
     int server_sock = socket(AF_INET, SOCK_STREAM, 0);
     if (server_sock < 0) {
@@ -38,7 +22,7 @@ int bind_and_listen() {
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_port = htons(port);
 
     // Bind the socket to the server address
     if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
@@ -54,7 +38,7 @@ int bind_and_listen() {
         return -1;
     }
 
-    printf("Server listening on port %d...\n", SERVER_PORT);
+    printf("Server listening on port %d...\n", port);
     return server_sock;
 }
 
@@ -97,70 +81,6 @@ FILE* read_config(int client_sock) {
     }
 
     return file;
-}
-
-// Parses JSON config file and returns cJSON object
-cJSON* parse_config(FILE* file) {
-    // Get file size
-    rewind(file);
-    char *json_string = NULL;
-    size_t json_len;
-    fseek(file, 0, SEEK_END);
-    json_len = ftell(file);
-    fseek(file, 0, SEEK_SET);
-
-    // Read file contents into string
-    json_string = malloc(json_len + 1);
-    if (json_string == NULL) {
-        perror("Error allocating JSON string");
-        fclose(file);
-        return NULL;
-    }
-    fread(json_string, 1, json_len, file);
-    json_string[json_len] = '\0';
-    
-    // Create cJSON object
-    cJSON *json_root = cJSON_Parse(json_string);
-    if (json_root == NULL) {
-        fprintf(stderr, "Error parsing JSON file.\n");
-        free(json_string);
-        fclose(file);
-        return NULL;
-    }
-
-    free(json_string);
-    return json_root;
-}
-
-// Prints ConfigData struct
-void print_config(ConfigData* config_data) {
-    printf("Server IP Address: %s\n", config_data->server_ip_addr);
-    printf("UDP Source Port: %d\n", config_data->udp_source_port);
-    printf("UDP Destination Port: %d\n", config_data->udp_destination_port);
-    printf("TCP Head SYN Port: %d\n", config_data->tcp_head_syn);
-    printf("TCP Tail SYN Port: %d\n", config_data->tcp_tail_syn);
-    printf("TCP Pre-Probe Port: %d\n", config_data->tcp_pre_probe);
-    printf("TCP Post-Probe Port: %d\n", config_data->tcp_post_probe);
-    printf("UDP Payload Size: %d\n", config_data->udp_payload_size);
-    printf("Inter-Measurement Time: %d\n", config_data->inter_measurement_time);
-    printf("Number of UDP Packets: %d\n", config_data->num_udp_packets);
-    printf("TTL: %d\n", config_data->ttl);
-}
-
-// Extracts config info from cJSON object and stores it in ConfigData struct
-void extract_config(ConfigData* config_data, const cJSON* json_root) {
-    strncpy(config_data->server_ip_addr, cJSON_GetObjectItem(json_root, "server_ip_addr")->valuestring, sizeof(config_data->server_ip_addr));
-    config_data->udp_source_port = cJSON_GetObjectItem(json_root, "udp_source_port")->valueint;
-    config_data->udp_destination_port = cJSON_GetObjectItem(json_root, "udp_destination_port")->valueint;
-    config_data->tcp_head_syn = cJSON_GetObjectItem(json_root, "tcp_head_syn")->valueint;
-    config_data->tcp_tail_syn = cJSON_GetObjectItem(json_root, "tcp_tail_syn")->valueint;
-    config_data->tcp_pre_probe = cJSON_GetObjectItem(json_root, "tcp_pre_probe")->valueint;
-    config_data->tcp_post_probe = cJSON_GetObjectItem(json_root, "tcp_post_probe")->valueint;
-    config_data->udp_payload_size = cJSON_GetObjectItem(json_root, "udp_payload_size")->valueint;
-    config_data->inter_measurement_time = cJSON_GetObjectItem(json_root, "inter_measurement_time")->valueint;
-    config_data->num_udp_packets = cJSON_GetObjectItem(json_root, "num_udp_packets")->valueint;
-    config_data->ttl = cJSON_GetObjectItem(json_root, "ttl")->valueint;
-    printf("JSON file received and parsed successfully.\n");
 }
 
 // Accepts tcp connections until config file data is successfully extracted
@@ -214,8 +134,10 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
+    int port = atoi(argv[1]);
+
     // Create socket and listen for connections
-    int server_sock = bind_and_listen();
+    int server_sock = bind_and_listen(port);
     if (server_sock < 0) {
         perror("Unable to create socket");
         return -1;
