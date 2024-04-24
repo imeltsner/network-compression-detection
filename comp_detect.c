c83bb81b30ad07b53c59ef9b7b71647a4c9b664f
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-04-24 13:28:11 -0700

    changed syn packet

--- comp_detect.c
+++ comp_detect.c
@@ -1,9 +1,3 @@
-/* 
-    This program uses code from P.D. Buchan for the raw socket implementation
-    Code can be found at https://www.pdbuchan.com/rawsock/tcp4.c
-    Copyright (C) 2011-2015  P.D. Buchan (pdbuchan@yahoo.com)
-*/ 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -24,425 +18,98 @@
 #include <sys/ioctl.h>        // macro ioctl is defined
 #include <bits/ioctls.h>      // defines values for argument "request" of ioctl.
 #include <net/if.h>           // struct ifreq
-
 #include <errno.h>            // errno, perror()
 
-// Define some constants.
-#define IP4_HDRLEN 20         // IPv4 header length
-#define TCP_HDRLEN 20         // TCP header length, excludes options data
-
-// P.D. Buchan raw socket implemnation starts here
-
-// Computing the internet checksum (RFC 1071).
-// Note that the internet checksum is not guaranteed to preclude collisions.
-uint16_t
-checksum (uint16_t *addr, int len) {
-
-  int count = len;
-  register uint32_t sum = 0;
-  uint16_t answer = 0;
-
-  // Sum up 2-byte values until none or only one byte left.
-  while (count > 1) {
-    sum += *(addr++);
-    count -= 2;
-  }
-
-  // Add left-over byte, if any.
-  if (count > 0) {
-    sum += *(uint8_t *) addr;
-  }
-
-  // Fold 32-bit sum into 16 bits; we lose information by doing this,
-  // increasing the chances of a collision.
-  // sum = (lower 16 bits) + (upper 16 bits shifted right 16 bits)
-  while (sum >> 16) {
-    sum = (sum & 0xffff) + (sum >> 16);
-  }
-
-  // Checksum is one's compliment of sum.
-  answer = ~sum;
-
-  return (answer);
+// Function to calculate the checksum of a packet
+unsigned short checksum(unsigned short *buf, int nwords) {
+    unsigned long sum;
+    for(sum=0; nwords>0; nwords--)
+        sum += *buf++;
+    sum = (sum >> 16) + (sum &0xffff);
+    sum += (sum >> 16);
+    return (unsigned short)(~sum);
 }
 
-// Build IPv4 TCP pseudo-header and call checksum function.
-uint16_t
-tcp4_checksum (struct ip iphdr, struct tcphdr tcphdr) {
+// Function to create and send a SYN packet
+void send_syn_packet(ConfigData* config_data, int destination_port) {
+    int sockfd;
+    struct sockaddr_in dest;
+    char packet[4096]; // Raw packet buffer
 
-  uint16_t svalue;
-  char buf[IP_MAXPACKET], cvalue;
-  char *ptr;
-  int chksumlen = 0;
-
-  // ptr points to beginning of buffer buf
-  ptr = &buf[0];
-
-  // Copy source IP address into buf (32 bits)
-  memcpy (ptr, &iphdr.ip_src.s_addr, sizeof (iphdr.ip_src.s_addr));
-  ptr += sizeof (iphdr.ip_src.s_addr);
-  chksumlen += sizeof (iphdr.ip_src.s_addr);
-
-  // Copy destination IP address into buf (32 bits)
-  memcpy (ptr, &iphdr.ip_dst.s_addr, sizeof (iphdr.ip_dst.s_addr));
-  ptr += sizeof (iphdr.ip_dst.s_addr);
-  chksumlen += sizeof (iphdr.ip_dst.s_addr);
-
-  // Copy zero field to buf (8 bits)
-  *ptr = 0; ptr++;
-  chksumlen += 1;
-
-  // Copy transport layer protocol to buf (8 bits)
-  memcpy (ptr, &iphdr.ip_p, sizeof (iphdr.ip_p));
-  ptr += sizeof (iphdr.ip_p);
-  chksumlen += sizeof (iphdr.ip_p);
-
-  // Copy TCP length to buf (16 bits)
-  svalue = htons (sizeof (tcphdr));
-  memcpy (ptr, &svalue, sizeof (svalue));
-  ptr += sizeof (svalue);
-  chksumlen += sizeof (svalue);
-
-  // Copy TCP source port to buf (16 bits)
-  memcpy (ptr, &tcphdr.th_sport, sizeof (tcphdr.th_sport));
-  ptr += sizeof (tcphdr.th_sport);
-  chksumlen += sizeof (tcphdr.th_sport);
-
-  // Copy TCP destination port to buf (16 bits)
-  memcpy (ptr, &tcphdr.th_dport, sizeof (tcphdr.th_dport));
-  ptr += sizeof (tcphdr.th_dport);
-  chksumlen += sizeof (tcphdr.th_dport);
-
-  // Copy sequence number to buf (32 bits)
-  memcpy (ptr, &tcphdr.th_seq, sizeof (tcphdr.th_seq));
-  ptr += sizeof (tcphdr.th_seq);
-  chksumlen += sizeof (tcphdr.th_seq);
-
-  // Copy acknowledgement number to buf (32 bits)
-  memcpy (ptr, &tcphdr.th_ack, sizeof (tcphdr.th_ack));
-  ptr += sizeof (tcphdr.th_ack);
-  chksumlen += sizeof (tcphdr.th_ack);
-
-  // Copy data offset to buf (4 bits) and
-  // copy reserved bits to buf (4 bits)
-  cvalue = (tcphdr.th_off << 4) + tcphdr.th_x2;
-  memcpy (ptr, &cvalue, sizeof (cvalue));
-  ptr += sizeof (cvalue);
-  chksumlen += sizeof (cvalue);
-
-  // Copy TCP flags to buf (8 bits)
-  memcpy (ptr, &tcphdr.th_flags, sizeof (tcphdr.th_flags));
-  ptr += sizeof (tcphdr.th_flags);
-  chksumlen += sizeof (tcphdr.th_flags);
-
-  // Copy TCP window size to buf (16 bits)
-  memcpy (ptr, &tcphdr.th_win, sizeof (tcphdr.th_win));
-  ptr += sizeof (tcphdr.th_win);
-  chksumlen += sizeof (tcphdr.th_win);
-
-  // Copy TCP checksum to buf (16 bits)
-  // Zero, since we don't know it yet
-  *ptr = 0; ptr++;
-  *ptr = 0; ptr++;
-  chksumlen += 2;
-
-  // Copy urgent pointer to buf (16 bits)
-  memcpy (ptr, &tcphdr.th_urp, sizeof (tcphdr.th_urp));
-  ptr += sizeof (tcphdr.th_urp);
-  chksumlen += sizeof (tcphdr.th_urp);
-
-  return checksum ((uint16_t *) buf, chksumlen);
-}
-
-// Allocate memory for an array of chars.
-char *
-allocate_strmem (int len) {
-
-  void *tmp;
-
-  if (len <= 0) {
-    fprintf (stderr, "ERROR: Cannot allocate memory because len = %i in allocate_strmem().\n", len);
-    exit (EXIT_FAILURE);
-  }
-
-  tmp = (char *) malloc (len * sizeof (char));
-  if (tmp != NULL) {
-    memset (tmp, 0, len * sizeof (char));
-    return (tmp);
-  } else {
-    fprintf (stderr, "ERROR: Cannot allocate memory for array allocate_strmem().\n");
-    exit (EXIT_FAILURE);
-  }
-}
-
-// Allocate memory for an array of unsigned chars.
-uint8_t *
-allocate_ustrmem (int len) {
-
-  void *tmp;
-
-  if (len <= 0) {
-    fprintf (stderr, "ERROR: Cannot allocate memory because len = %i in allocate_ustrmem().\n", len);
-    exit (EXIT_FAILURE);
-  }
-
-  tmp = (uint8_t *) malloc (len * sizeof (uint8_t));
-  if (tmp != NULL) {
-    memset (tmp, 0, len * sizeof (uint8_t));
-    return (tmp);
-  } else {
-    fprintf (stderr, "ERROR: Cannot allocate memory for array allocate_ustrmem().\n");
-    exit (EXIT_FAILURE);
-  }
-}
-
-// Allocate memory for an array of ints.
-int *
-allocate_intmem (int len) {
-
-  void *tmp;
-
-  if (len <= 0) {
-    fprintf (stderr, "ERROR: Cannot allocate memory because len = %i in allocate_intmem().\n", len);
-    exit (EXIT_FAILURE);
-  }
-
-  tmp = (int *) malloc (len * sizeof (int));
-  if (tmp != NULL) {
-    memset (tmp, 0, len * sizeof (int));
-    return (tmp);
-  } else {
-    fprintf (stderr, "ERROR: Cannot allocate memory for array allocate_intmem().\n");
-    exit (EXIT_FAILURE);
-  }
-}
-
-int send_raw_socket(ConfigData *config_data, int destination_port) {
-    int i, status, sd, *ip_flags, *tcp_flags;
-    const int on = 1;
-    char *interface, *target, *src_ip, *dst_ip;
-    struct ip iphdr;
-    struct tcphdr tcphdr;
-    uint8_t *packet;
-    struct addrinfo hints, *res;
-    struct sockaddr_in *ipv4, sin;
-    struct ifreq ifr;
-    void *tmp;
-
-    // Allocate memory for various arrays.
-    packet = allocate_ustrmem (IP_MAXPACKET);
-    interface = allocate_strmem (40);
-    target = allocate_strmem (40);
-    src_ip = allocate_strmem (INET_ADDRSTRLEN);
-    dst_ip = allocate_strmem (INET_ADDRSTRLEN);
-    ip_flags = allocate_intmem (4);
-    tcp_flags = allocate_intmem (8);
-
-    // Interface to send packet through.
-    strcpy (interface, "enp0s1");
-
-    // Submit request for a socket descriptor to look up interface.
-    if ((sd = socket (AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0) {
-        perror ("socket() failed to get socket descriptor for using ioctl() ");
-        return (1);
+    // Create raw socket
+    if ((sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
     }
 
-    // Use ioctl() to look up interface index which we will use to
-    // bind socket descriptor sd to specified interface with setsockopt() since
-    // none of the other arguments of sendto() specify which interface to use.
-    memset (&ifr, 0, sizeof (ifr));
-    snprintf (ifr.ifr_name, sizeof (ifr.ifr_name), "%s", interface);
-    if (ioctl (sd, SIOCGIFINDEX, &ifr) < 0) {
-        perror ("ioctl() failed to find interface ");
-        return (1);
-    }
-    close (sd);
+    // Zero out the packet buffer
+    memset(packet, 0, sizeof(packet));
 
-    // Source IPv4 address: you need to fill this out
-    strcpy (src_ip, "0.0.0.0");
-
-    // Destination URL or IPv4 address: you need to fill this out
-    char destinationPortStr[20];
-    snprintf(destinationPortStr, sizeof(destinationPortStr), "%d", destination_port);
-    strcpy (target, destinationPortStr);
-
-    // Fill out hints for getaddrinfo().
-    memset (&hints, 0, sizeof (struct addrinfo));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = hints.ai_flags | AI_CANONNAME;
-
-    // Resolve target using getaddrinfo().
-    if ((status = getaddrinfo (target, NULL, &hints, &res)) != 0) {
-        fprintf (stderr, "getaddrinfo() failed for target: %s\n", gai_strerror (status));
-        return (1);
-    }
-    ipv4 = (struct sockaddr_in *) res->ai_addr;
-    tmp = &(ipv4->sin_addr);
-    if (inet_ntop (AF_INET, tmp, dst_ip, INET_ADDRSTRLEN) == NULL) {
-        status = errno;
-        fprintf (stderr, "inet_ntop() failed for target.\nError message: %s", strerror (status));
-        exit (EXIT_FAILURE);
-    }
-    freeaddrinfo (res);
-
-    // IPv4 header
-    // IPv4 header length (4 bits): Number of 32-bit words in header = 5
-    iphdr.ip_hl = IP4_HDRLEN / sizeof (uint32_t);
-    // Internet Protocol version (4 bits): IPv4
-    iphdr.ip_v = 4;
-    // Type of service (8 bits)
-    iphdr.ip_tos = 0;
-    // Total length of datagram (16 bits): IP header + TCP header
-    iphdr.ip_len = htons (IP4_HDRLEN + TCP_HDRLEN);
-    // ID sequence number (16 bits): unused, since single datagram
-    iphdr.ip_id = htons (0);
-    // Flags, and Fragmentation offset (3, 13 bits): 0 since single datagram
-    // Zero (1 bit)
-    ip_flags[0] = 0;
-    // Do not fragment flag (1 bit)
-    ip_flags[1] = 0;
-    // More fragments following flag (1 bit)
-    ip_flags[2] = 0;
-    // Fragmentation offset (13 bits)
-    ip_flags[3] = 0;
-    iphdr.ip_off = htons ((ip_flags[0] << 15)
-                        + (ip_flags[1] << 14)
-                        + (ip_flags[2] << 13)
-                        +  ip_flags[3]);
-    // Time-to-Live (8 bits): default to maximum value
-    iphdr.ip_ttl = 255;
-    // Transport layer protocol (8 bits): 6 for TCP
-    iphdr.ip_p = IPPROTO_TCP;
-    // Source IPv4 address (32 bits)
-    if ((status = inet_pton (AF_INET, src_ip, &(iphdr.ip_src))) != 1) {
-        fprintf (stderr, "inet_pton() failed for source address.\nError message: %s", strerror (status));
-        exit (1);
-    }
-
-
-    const char* destinationIP = config_data->server_ip_addr;
-    // Destination IPv4 address (32 bits)
-    if ((status = inet_pton (AF_INET, destinationIP, &(iphdr.ip_dst))) != 1) {
-        fprintf (stderr, "inet_pton() failed for destination address.\nError message: %s", strerror (status));
-        exit (1);
-    }
-
-    // IPv4 header checksum (16 bits): set to 0 when calculating checksum
-    iphdr.ip_sum = 0;
-    iphdr.ip_sum = checksum ((uint16_t *) &iphdr, IP4_HDRLEN);
+    // IP header
+    struct iphdr *ip = (struct iphdr *)packet;
+    ip->ihl = 5;
+    ip->version = 4;
+    ip->tos = 0;
+    ip->tot_len = htons(sizeof(struct iphdr) + sizeof(struct tcphdr));
+    ip->id = htons(54321);
+    ip->frag_off = 0;
+    ip->ttl = 255;
+    ip->protocol = IPPROTO_TCP;
+    ip->check = 0; // Will be filled later
+    ip->saddr = inet_addr("0.0.0.0");
+    ip->daddr = inet_addr(config_data->server_ip_addr);
 
     // TCP header
-    // Source port number (16 bits)
-    tcphdr.th_sport = htons (1234);
-    // Destination port number (16 bits)
-    tcphdr.th_dport = htons (destination_port);
+    struct tcphdr *tcp = (struct tcphdr *)(packet + sizeof(struct iphdr));
+    tcp->source = htons(12345);
+    tcp->dest = htons(destination_port);
+    tcp->seq = 0;
+    tcp->ack_seq = 0;
+    tcp->doff = 5;
+    tcp->fin = 0;
+    tcp->syn = 1; // Set SYN flag
+    tcp->rst = 0;
+    tcp->psh = 0;
+    tcp->ack = 0;
+    tcp->urg = 0;
+    tcp->window = htons(5840);
+    tcp->check = 0; // Will be filled later
+    tcp->urg_ptr = 0;
 
-    // Sequence number (32 bits)
-    tcphdr.th_seq = htonl (0);
+    // IP checksum
+    ip->check = checksum((unsigned short *)packet, ip->ihl * 4);
 
-    // Acknowledgement number (32 bits): 0 in first packet of SYN/ACK process
-    tcphdr.th_ack = htonl (1);
+    // TCP pseudo-header
+    struct pseudo_header {
+        unsigned int source_address;
+        unsigned int dest_address;
+        unsigned char placeholder;
+        unsigned char protocol;
+        unsigned short tcp_length;
+    } pseudo_header;
 
-    // Reserved (4 bits): should be 0
-    tcphdr.th_x2 = 0;
+    pseudo_header.source_address = inet_addr("0.0.0.0");
+    pseudo_header.dest_address = inet_addr(config_data->server_ip_addr);
+    pseudo_header.placeholder = 0;
+    pseudo_header.protocol = IPPROTO_TCP;
+    pseudo_header.tcp_length = htons(sizeof(struct tcphdr));
 
-    // Data offset (4 bits): size of TCP header in 32-bit words
-    tcphdr.th_off = TCP_HDRLEN / 4;
+    // TCP checksum
+    int packet_length = sizeof(struct pseudo_header) + sizeof(struct tcphdr);
+    char *pseudo_packet = malloc(packet_length);
+    memcpy(pseudo_packet, (char *)&pseudo_header, sizeof(struct pseudo_header));
+    memcpy(pseudo_packet + sizeof(struct pseudo_header), tcp, sizeof(struct tcphdr));
+    tcp->check = checksum((unsigned short *)pseudo_packet, packet_length / 2);
+    free(pseudo_packet);
 
-    // Flags (8 bits)
+    // Send packet
+    dest.sin_family = AF_INET;
+    dest.sin_addr.s_addr = ip->daddr;
+    sendto(sockfd, packet, ip->tot_len, 0, (struct sockaddr *)&dest, sizeof(dest));
 
-    // FIN flag (1 bit)
-    tcp_flags[0] = 0;
-
-    // SYN flag (1 bit): set to 1
-    tcp_flags[1] = 1;
-
-    // RST flag (1 bit)
-    tcp_flags[2] = 0;
-
-    // PSH flag (1 bit)
-    tcp_flags[3] = 0;
-
-    // ACK flag (1 bit)
-    tcp_flags[4] = 0;
-
-    // URG flag (1 bit)
-    tcp_flags[5] = 0;
-
-    // ECE flag (1 bit)
-    tcp_flags[6] = 0;
-
-    // CWR flag (1 bit)
-    tcp_flags[7] = 0;
-
-    tcphdr.th_flags = 0;
-    for (i=0; i<8; i++) {
-        tcphdr.th_flags += (tcp_flags[i] << i);
-    }
-
-    // Window size (16 bits)
-    tcphdr.th_win = htons (65535);
-
-    // Urgent pointer (16 bits): 0 (only valid if URG flag is set)
-    tcphdr.th_urp = htons (0);
-
-    // TCP checksum (16 bits)
-    tcphdr.th_sum = tcp4_checksum (iphdr, tcphdr);
-
-    // Prepare packet.
-
-    // First part is an IPv4 header.
-    memcpy (packet, &iphdr, IP4_HDRLEN * sizeof (uint8_t));
-
-    // Next part of packet is upper layer protocol header.
-    memcpy ((packet + IP4_HDRLEN), &tcphdr, TCP_HDRLEN * sizeof (uint8_t));
-
-    // The kernel is going to prepare layer 2 information (ethernet frame header) for us.
-    // For that, we need to specify a destination for the kernel in order for it
-    // to decide where to send the raw datagram. We fill in a struct in_addr with
-    // the desired destination IP address, and pass this structure to the sendto() function.
-    memset (&sin, 0, sizeof (struct sockaddr_in));
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = iphdr.ip_dst.s_addr;
-
-    // Submit request for a raw socket descriptor.
-    if ((sd = socket (AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0) {
-        perror ("socket() failed ");
-        exit (1);
-    }
-
-    // Set flag so socket expects us to provide IPv4 header.
-    if (setsockopt (sd, IPPROTO_IP, IP_HDRINCL, &on, sizeof (on)) < 0) {
-        perror ("setsockopt() failed to set IP_HDRINCL ");
-        exit (1);
-    }
-
-    // Bind socket to interface index.
-    if (setsockopt (sd, SOL_SOCKET, SO_BINDTODEVICE, &ifr, sizeof (ifr)) < 0) {
-        perror ("setsockopt() failed to bind to interface ");
-        exit (1);
-    }
-
-    // Send packet.
-    if (sendto (sd, packet, IP4_HDRLEN + TCP_HDRLEN, 0, (struct sockaddr *) &sin, sizeof (struct sockaddr)) < 0)  {
-        perror ("sendto() failed ");
-        exit (1);
-    }
-
-    // Close socket descriptor.
-    close (sd);
-
-    // Free allocated memory.
-    free (packet);
-    free (interface);
-    free (target);
-    free (src_ip);
-    free (dst_ip);
-    free (ip_flags);
-    free (tcp_flags);
+    // Close socket
+    close(sockfd);
 }
-// P.D. Buchan raw socket implementation ends here
 
 // Reads a parses a config file
 ConfigData* get_config_data(char *file_path) {
@@ -595,9 +262,9 @@ int main(int argc, char *argv[]) {
     ConfigData* config_data = get_config_data(file_path);
 
     // Send packets
-    send_raw_socket(config_data, config_data->tcp_head_syn);
+    send_syn_packet(config_data, config_data->tcp_head_syn);
     send_udp_packets(config_data);
-    send_raw_socket(config_data, config_data->tcp_tail_syn);
+    send_syn_packet(config_data, config_data->tcp_tail_syn);
 
     free(config_data);
     return 0;
