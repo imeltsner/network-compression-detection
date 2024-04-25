1dfca500ebcbd0b06bc7489555cc11c7b3059c54
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-04-24 20:21:43 -0700

    changed sleep to 8

--- client.c
+++ client.c
@@ -246,7 +246,7 @@ int main(int argc, char *argv[]) {
     // Send first packet trains
     send_udp_packets(config_data);
     
-    sleep(1);
+    sleep(8);
 
     // Print compression message
     receive_compression_message(config_data);
